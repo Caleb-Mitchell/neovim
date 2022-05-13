c09e30275a7fcb439f17d7cad1a9e0d9ca5c187f
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-05-13 14:30:52 -0500

    Set colorscheme to gruvbox

--- lua/caleb/colorscheme.lua
+++ lua/caleb/colorscheme.lua
@@ -1,8 +1,8 @@
 vim.cmd [[
 try
   " let g:gruvbox_baby_background_color = 'dark'
-  let g:gruvbox_baby_transparent_mode = 1
-  colorscheme gruvbox-baby
+  " let g:gruvbox_baby_transparent_mode = 1
+  colorscheme gruvbox
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
