fb03ac168fe51e85bc766165e6cf117320862129
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-06-18 17:32:59 -0500

    Set colorscheme to gruvbox with grey colorcolumn

--- lua/caleb/colorscheme.lua
+++ lua/caleb/colorscheme.lua
@@ -1,10 +1,9 @@
 vim.cmd [[
 try
   " let g:gruvbox_baby_background_color = 'dark'
-  let g:gruvbox_baby_transparent_mode = 1
-  colorscheme gruvbox-baby
-  " highlight ColorColumn guibg=#32302F
-  highlight ColorColumn guibg=#d3d3d3
+  " let g:gruvbox_baby_transparent_mode = 1
+  colorscheme gruvbox
+  highlight ColorColumn guibg=grey
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
