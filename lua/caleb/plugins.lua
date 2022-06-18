7091c61c3d538a26bf9587a734b2804f92ec48a9
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-06-18 14:43:19 -0500

    Add commented line to limit number of jobs in plugs if bad internet connection

--- lua/caleb/plugins.lua
+++ lua/caleb/plugins.lua
@@ -31,6 +31,7 @@ end
 
 -- Have packer use a popup window
 packer.init {
+  -- max_jobs = 1, -- in case of bad internet connection
   display = {
     open_fn = function()
       return require("packer.util").float { border = "rounded" }
