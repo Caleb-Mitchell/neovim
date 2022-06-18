1c4f4d2830c7cfc8213bb51f775df9094782a2fa
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-06-18 17:05:44 -0500

    Update colorcolumn color to light grey

--- lua/caleb/colorscheme.lua
+++ lua/caleb/colorscheme.lua
@@ -3,7 +3,8 @@ try
   " let g:gruvbox_baby_background_color = 'dark'
   let g:gruvbox_baby_transparent_mode = 1
   colorscheme gruvbox-baby
-  highlight ColorColumn guibg=#32302F
+  " highlight ColorColumn guibg=#32302F
+  highlight ColorColumn guibg=#d3d3d3
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
