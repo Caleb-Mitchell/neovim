f02106d88cc1e68c9d909488fa2f50e081b4055b
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-06-18 17:46:08 -0500

    Remove transparent plugin, switch back to gruvbox-baby colorscheme with transparent mode

--- lua/caleb/plugins.lua
+++ lua/caleb/plugins.lua
@@ -84,7 +84,6 @@ return packer.startup(function(use)
   use "ellisonleao/gruvbox.nvim"
   use "luisiacc/gruvbox-baby"
 
-  use "xiyaowong/nvim-transparent"
   use 'folke/lsp-colors.nvim' -- Adds color support for lsp highlighting if needed
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
 
