7cdb61f3f0052b4b9230c618d7802bf0b66f8548
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-06-29 10:28:55 -0500

    Change colorscheme to make quickfix more readable

--- lua/caleb/colorscheme.lua
+++ lua/caleb/colorscheme.lua
@@ -4,6 +4,7 @@ try
   let g:gruvbox_baby_transparent_mode = 1
   colorscheme gruvbox-baby
   highlight ColorColumn guibg=grey
+  highlight QuickFixLine guibg=black
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
