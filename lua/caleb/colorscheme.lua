5f995b4ae263370a61549342fbe9a34470bfe544
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-09-01 15:03:09 -0500

    Update colorscheme so that the font of unselected tabs is white and more visible

--- lua/caleb/colorscheme.lua
+++ lua/caleb/colorscheme.lua
@@ -3,7 +3,7 @@ try
   " let g:gruvbox_baby_background_color = 'dark'
   let g:gruvbox_baby_transparent_mode = 1
   colorscheme gruvbox-baby
-  highlight Tabline guibg=#fff
+  highlight Tabline guibg=#fff guifg=white
   highlight ColorColumn guibg=grey
   highlight QuickFixLine guibg=black
 catch /^Vim\%((\a\+)\)\=:E185/
