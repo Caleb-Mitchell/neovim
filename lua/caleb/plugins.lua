5e0c0e5cffb37373c323dfdea17060100f82f232
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-10-20 18:40:41 -0500

    Clean up extra comment

--- lua/caleb/plugins.lua
+++ lua/caleb/plugins.lua
@@ -73,7 +73,7 @@ return packer.startup(function(use)
         use_lsp = true, -- Use the builtin LSP to show errors and warnings
         use_treesitter = true, -- Use nvim-treesitter to highlight the code
         exclude_filetypes = {}, -- Choose certain filetypes to not show minimap on
-        z_index = 1, -- The z-index the floating window will be on       exclude_filetypes = {"nvimtree_1"},
+        z_index = 1, -- The z-index the floating window will be on
       })
       codewindow.apply_default_keybinds()
     end,
