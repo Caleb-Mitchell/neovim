797303feb8375a8416ad5eabbdf70d533e5a81c6
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-10-25 20:37:07 -0500

    Turn off minimap autolaunch as it is annoying with splits

--- lua/caleb/plugins.lua
+++ lua/caleb/plugins.lua
@@ -69,7 +69,7 @@ return packer.startup(function(use)
 			local codewindow = require("codewindow")
 			codewindow.setup({
 				active_in_terminals = false, -- Should the minimap activate for terminal buffers
-				auto_enable = true, -- Automatically open the minimap when entering a (non-excluded) buffer (accepts a table of filetypes)
+				auto_enable = false, -- Automatically open the minimap when entering a (non-excluded) buffer (accepts a table of filetypes)
 				exclude_filetypes = { "NvimTree", "alpha", "packer" }, -- Choose certain filetypes to not show minimap on
 			})
 			codewindow.apply_default_keybinds()
