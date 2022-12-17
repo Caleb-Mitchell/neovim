d4b7d937af9d4d47b8b09efa591cf66d8a374c20
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-12-17 11:50:10 -0600

    Tab will now interact with cmp, but not enter by default

--- lua/caleb/cmp.lua
+++ lua/caleb/cmp.lua
@@ -69,9 +69,9 @@ cmp.setup {
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = false },
     ["<Tab>"] = cmp.mapping(function(fallback)
-      -- if cmp.visible() then
-      --   cmp.select_next_item()
-      if luasnip.expandable() then
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
         luasnip.expand()
       -- elseif luasnip.expand_or_jumpable() then
       --   luasnip.expand_or_jump()
