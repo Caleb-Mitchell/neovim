54d6c9d7b75449199d80f91020869df316cec707
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2022-12-26 19:59:50 -0600

    Disable treesitter syntax highlighting for sql, because it is not PosgreSQL specific

--- lua/caleb/treesitter.lua
+++ lua/caleb/treesitter.lua
@@ -13,8 +13,8 @@ configs.setup {
   },
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
+    disable = { "sql" }, -- list of language that will be disabled
+    -- additional_vim_regex_highlighting = true,
   },
   indent = { enable = true, disable = { "yaml", "ruby", "html" } },
   -- Configuration for context-commentstring plugin
@@ -29,5 +29,8 @@ configs.setup {
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
+  },
+  playground = {
+    enable = true
   }
 }
