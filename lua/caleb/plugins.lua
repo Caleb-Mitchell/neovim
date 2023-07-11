80b4bf1170708f0406ca4602043cadd58e58c11f
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-07-11 18:25:34 -0500

    Update snippet use block to ensure lazyloading of custom vscode style snippets

--- lua/caleb/plugins.lua
+++ lua/caleb/plugins.lua
@@ -154,7 +154,12 @@ return packer.startup(function(use)
   -- }
 
   -- snippets
-  use("L3MON4D3/LuaSnip") --snippet engine
+  use({
+    'L3MON4D3/LuaSnip',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load() -- autoload my custom snippets that are vscode-like
+    end,
+  })
   use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
   -- LSP
