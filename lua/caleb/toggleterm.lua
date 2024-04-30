4573cc1526660f5cff738a8eddebac2540599496
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2024-04-29 22:52:53 -0500

    Configure toggle term to use default location along bottom border

--- lua/caleb/toggleterm.lua
+++ lua/caleb/toggleterm.lua
@@ -5,7 +5,7 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
+	-- size = 20,
 	open_mapping = [[<c-t>]],
 	hide_numbers = true,
 	shade_filetypes = {},
@@ -14,7 +14,7 @@ toggleterm.setup({
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "float",
+	-- direction = "float",
 	close_on_exit = true,
 	shell = vim.o.shell,
 	float_opts = {
