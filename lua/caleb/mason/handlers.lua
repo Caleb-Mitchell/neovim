5e955137f02c64a1def3c82a863fc34858b4cf33
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2024-05-08 16:48:46 -0500

    Turn off tsserver linting in favor of eslint linting

--- lua/caleb/mason/handlers.lua
+++ lua/caleb/mason/handlers.lua
@@ -86,6 +86,7 @@ end
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.server_capabilities.document_formatting = false
+    client.server_capabilities.document_linting = false
   end
   if client.name == "sumneko_lua" then
     client.server_capabilities.document_formatting = false
